3edc45262ce4ef7e80939bff383d74c11db62d6a
JulianeMonteiro <51388071+JulianeMonteiro@users.noreply.github.com>
2021-07-08 01:23:08 -0300

    Initial commit

---
+++ app/src/main/java/com/juliane/conheca_me/FragmentExperiencia.kt
@@ -0,0 +1,22 @@
+package com.juliane.conheca_me
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FragmentExperiencia : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_experiencia, container, false)
+    }
+}
